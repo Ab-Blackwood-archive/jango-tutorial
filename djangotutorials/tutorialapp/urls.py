4e70521954dcbfc2b43cdb6ff9db7495f6073182
Ab-Blackwood-archive <ab061906>
2024-04-17 10:18:35 -0500

    final work

--- djangotutorials/tutorialapp/urls.py
+++ djangotutorials/tutorialapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('base/', views.base, name='base'),
     path('',views.home, name='home'),
     path('students/',views.Students, name='students'),
-    path('teachers',views.Teachers, name='teachers'),
+    path('teachers/',views.Teachers, name='teachers'),
 ]
