6aea53bb256ef09045623e1b287040fd748117d3
Ab-Blackwood-archive <ab061906>
2024-04-29 11:09:42 -0500

    addded inage static

--- djangotutorials/tutorialapp/urls.py
+++ djangotutorials/tutorialapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('base/', views.base, name='base'),
@@ -7,5 +9,5 @@ urlpatterns = [
     path('students/',views.Students, name='students'),
     path('teachers/',views.Teachers, name='teachers'),
     path('teacherform/',views.teacherform, name='teacherform'),
-    path('studentform/', views.studentform, name='studentform')
-]
+    path('studentform/', views.studentform, name='studentform'),
+]+static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
